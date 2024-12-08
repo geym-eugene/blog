7d0805e8cbad0053318ceba2c40963189cb7c4f4
Eugene Geym <geymzheka@gmail.com>
2024-12-08 20:17:46 +0500

    added: admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from blog.models import Post, Comment
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "text", "author", "created_at", "updated_at")
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("author", "text", "get_post_title", "created_at", "updated_at")
+
+    def get_post_title(self, obj):
+        return obj.post.title
+
+    get_post_title.short_description = "Пост"
